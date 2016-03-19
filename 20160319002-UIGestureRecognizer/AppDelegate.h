9628e7de2f9226e07453fc6f354a230072e91d87
raineryue <it_jinlei.yue@163.com>
2016-03-19 19:28:22 +0800

    Initial Commit

---
+++ 20160319002-UIGestureRecognizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160319002-UIGestureRecognizer
+//
+//  Created by Rainer on 16/3/19.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160319002-UIGestureRecognizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160319002-UIGestureRecognizer
+//
+//  Created by Rainer on 16/3/19.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
